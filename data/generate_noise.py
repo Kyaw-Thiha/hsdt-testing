5fab322aa80f047efaf47b01454f2cadcb4d7c1c
Kevin_KyawThiha <kyaw.thiha@mail.utoronto.ca>
2025-06-16 15:54:35 -0400

    Updated

--- data/generate_noise.py
+++ data/generate_noise.py
@@ -7,15 +7,18 @@ import numpy as np
 
 
 def add_gaussian_noise(input_dir: str, output_dir: str, sigma: int):
+    print("adding noise")
     os.makedirs(output_dir, exist_ok=True)
     mat_files = glob(os.path.join(input_dir, "*.mat"))
     i = 0
+    print("go")
 
     for mat_path in mat_files:
         print(f"Generating Image-{i} for sigma-{sigma}")
         with h5py.File(mat_path, "r") as f:
             key = list(f.keys())[0]  # assume the dataset is the first key
             img = np.array(f[key]).astype(np.float32)
+            # print(f"{os.path.basename(mat_path)} shape: {img.shape}")
             img = img.transpose(
                 2, 1, 0
             )  # MATLAB to NumPy: (bands, cols, rows) → (rows, cols, bands)
