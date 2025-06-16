d13df50e1fcc18c059510fb2f941d3cb660a4195
Kevin_KyawThiha <kyaw.thiha@mail.utoronto.ca>
2025-06-15 22:39:35 -0400

    Edited the generate noise file

--- data/generate_noise.py
+++ data/generate_noise.py
@@ -1,15 +1,40 @@
 import os
 
-import glob
+from glob import glob
 import scipy
+import h5py
 import numpy as np
 
-for sigma in [30, 50]:
-    os.makedirs(f"icvl_{sigma}", exist_ok=True)
+
+def add_gaussian_noise(input_dir: str, output_dir: str, sigma: int):
+    os.makedirs(output_dir, exist_ok=True)
+    mat_files = glob(os.path.join(input_dir, "*.mat"))
     i = 0
-    for f in glob.glob("icvl_clean/*.mat"):
+
+    for mat_path in mat_files:
         print(f"Generating Image-{i} for sigma-{sigma}")
-        data = scipy.io.loadmat(f)["img"]
-        noisy = data + np.random.randn(*data.shape) * sigma
-        scipy.io.savemat(f"icvl_{sigma}/{os.path.basename(f)}", {"img": noisy})
+        with h5py.File(mat_path, "r") as f:
+            key = list(f.keys())[0]  # assume the dataset is the first key
+            img = np.array(f[key]).astype(np.float32)
+            img = img.transpose(
+                2, 1, 0
+            )  # MATLAB to NumPy: (bands, cols, rows) → (rows, cols, bands)
+
+        noisy = img + np.random.normal(0, sigma, img.shape).astype(np.float32)
+
+        save_path = os.path.join(output_dir, os.path.basename(mat_path))
+        scipy.io.savemat(save_path, {key: noisy})
         i = i + 1
+
+
+print("Generating noisy images of Sigma-30")
+add_gaussian_noise("icvl_clean", "icvl_30", sigma=30)
+print("-------------------------------------")
+
+print("Generating noisy images of Sigma-50")
+add_gaussian_noise("icvl_clean", "icvl_50", sigma=50)
+print("-------------------------------------")
+
+print("Generating noisy images of Sigma-70")
+add_gaussian_noise("icvl_clean", "icvl_70", sigma=70)
+print("-------------------------------------")
